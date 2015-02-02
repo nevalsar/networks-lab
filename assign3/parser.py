552af9eb47a226886b513396e5eef9ada5e8ee04
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-02-02 22:33:46 +0530

    Modify program to run 10 samples for 16-1024 datarates and calculate average values of delay and throughput, and jitter

--- assign3/parser.py
+++ assign3/parser.py
@@ -8,60 +8,77 @@
 #!/bin/env python
 import sys
 from operator import itemgetter
+import math
+import numpy as np
 
-if len(sys.argv) < 2:
-	print "Parameters : data file and packetsize(defaults to 128 bytes)"
-	exit
-elif len(sys.argv) == 2:
-	packetSize = 128
-else:
-	packetSize = int(sys.argv[2])
+def getCSMAParams(rawData, packetSize):
+	data = []
+	for line in rawData:
+		if('udp' in line.strip().lower()):
+			data.append(line.strip().split())
 
-rawData = open(sys.argv[1])
-data = []
+	data[:] = [[element[0], element[1], int(element[6][22:-1]), int(element[7][27:-1])] for element in data]
 
-print "\nStatistics : \n"
+	groupedData_keys = [(element[2], element[3]) for element in data]
+	groupedData = dict.fromkeys(groupedData_keys)
+	packetType_keys = ['+', '-', 'r', 'delay', 'ifReceived']
 
-for line in rawData:
-	if('udp' in line.strip().lower()):
-		data.append(line.strip().split())
+	for element in data:
+		key = (element[2], element[3])
+		if groupedData[key] is None:
+			groupedData[key] = dict.fromkeys(packetType_keys)
+		groupedData[key][element[0]] = element[1]
 
-data[:] = [[element[0], element[1], int(element[6][22:-1]), int(element[7][27:-1])] for element in data]
+	for key in groupedData:
+		element = groupedData[key]
+		if element['+'] is not None:
+			if element['r'] is not None:
+				element['ifReceived'] = True
+				element['delay'] = float(element['r']) - float(element['+'])
+			else:
+				element['ifReceived'] = False
 
-groupedData_keys = [(element[2], element[3]) for element in data]
-groupedData = dict.fromkeys(groupedData_keys)
-packetType_keys = ['+', '-', 'r', 'delay', 'ifReceived']
+	temp = [element for element in data if element[0] == '+']
+	sentPackets = sorted(temp, key = itemgetter(1))
+	if len(sentPackets) < 1:
+		print "No sent"
+		sys.exit()
+	firstTranmissionTime = float(sentPackets[0][1])
 
-for element in data:
-	key = (element[2], element[3])
-	if groupedData[key] is None:
-		groupedData[key] = dict.fromkeys(packetType_keys)
-	groupedData[key][element[0]] = element[1]
+	temp = [element for element in data if element[0] == 'r']
+	receivedPackets = sorted(temp, key = itemgetter(1))
+	lastReceivedTime = float(sentPackets[-1][1])
 
-for key in groupedData:
-	element = groupedData[key]
-	if element['+'] is not None:
-		if element['r'] is not None:
-			element['ifReceived'] = True
-			element['delay'] = float(element['r']) - float(element['+'])
-		else:
-			element['ifReceived'] = False
+	totalTime = lastReceivedTime - firstTranmissionTime
 
-temp = [element for element in data if element[0] == '+']
-sentPackets = sorted(temp, key = itemgetter(1))
-firstTranmissionTime = float(sentPackets[0][1])
+	temp = [groupedData[key] for key in groupedData if groupedData[key]['ifReceived'] is not None]
+	totalBytesReceived = packetSize * len(temp)
+	throughPut = totalBytesReceived/totalTime
+	# print "Throughput = " + str(throughPut) + " bytes/second"
 
-temp = [element for element in data if element[0] == 'r']
-receivedPackets = sorted(temp, key = itemgetter(1))
-lastReceivedTime = float(sentPackets[-1][1])
+	delays = [groupedData[key]['delay'] for key in groupedData if groupedData[key]['ifReceived'] is not None]
+	totalDelay = sum(delays)
+	# print "Total delay = " + str(totalDelay) + " seconds"
 
-totalTime = lastReceivedTime - firstTranmissionTime
+	return (throughPut, totalDelay)
 
-temp = [groupedData[key] for key in groupedData if groupedData[key]['ifReceived'] is not None]
-totalBytesReceived = packetSize * len(temp)
-throughPut = totalBytesReceived/totalTime
-print "Throughput = " + str(throughPut) + " bytes/second"
+for power in range(4, 11):
+	dataRate = int(math.pow(2, power))
+	packetSize = dataRate / 4
+	csmaValues = []
+	for i in range(1, 11):
+		fileName = "csma_" + str(dataRate) + "_" + str(i) + ".tr"
+		rawData = open(fileName, 'r')
+		print dataRate, i
+		csmaValues.append(getCSMAParams(rawData, packetSize))
+	throughPuts = [element[0] for element in csmaValues]
+	delays = [element[1] for element in csmaValues]
+	avgDelay = float(sum(delays))/10
+	avgThroughPut = float(sum(throughPuts))/10
+	jitter = np.std(delays)
 
-delays = [groupedData[key]['delay'] for key in groupedData if groupedData[key]['ifReceived'] is not None]
-totalDelay = sum(delays)
-print "Total delay = " + str(totalDelay) + " seconds"
+	print "Data Rate \t= " + str(dataRate)
+	print "Avg Throughput \t= " + str(avgThroughPut)
+	print "Avg Delay \t= " + str(avgDelay)
+	print "Jitter \t\t= " + str(jitter)
+	print
