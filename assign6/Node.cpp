bbf15d4ce3f7068325ad31e216427f4a79ad8e69
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-04-05 00:25:14 +0530

    Rewrite Socket.cpp to add new functions
    - Add send_tcp_file, receive and send tcp string
    - Incorporate send and receive udp string functions under class
    - Modify Node.cpp to suit modified Socket.cpp
    
    Signed-off-by: Nevin Valsaraj <nevin.valsaraj32@gmail.com>

--- assign6/Node.cpp
+++ assign6/Node.cpp
@@ -93,21 +93,12 @@ class Node{
 
 Node::Node(int n) {
     node_count = n;
+    Socket sockobj;
     // get own ip and port, generate own id / hash value
-    Socket new_conn;
-    new_conn.ifUDP = true;
-    new_conn.ifRemote = false;
-    new_conn.port.assign(std::to_string(ROOT_PORT + node_count));
-    new_conn.getaddressinformation();
-    new_conn.makesocket(false, false);
-    ip.assign(new_conn.get_ip());
+    ip.assign(sockobj.get_own_ip());
     port.assign(std::to_string(ROOT_PORT + node_count));
-    new_conn.cleardata();
-    close(new_conn.sockfd);
-    string temp;
-    temp.assign(ip);
-    temp.push_back(':');
-    temp.append(port);
+    string temp(ip + ":" + port);
+    cout <<"Start node : " <<temp <<endl;
     id = hash_fn(temp);
     successor = NULL;
 
@@ -125,7 +116,7 @@ Node::Node(int n) {
     }
 
     // set up listener sockets
-    if(node_count == 0)
+    if (node_count == 0)
         setup_sockets(true);
     else
         setup_sockets(false);
@@ -249,94 +240,39 @@ Node* Node::getpointer_to_node(std::size_t node_id, Node* startptr) {
 // }
 
 void Node::send_files_list(std::vector<string> files) {
-    int sockfd, numbytes;
-    Socket new_connection;
-    new_connection.ifUDP = true;
-    new_connection.ifRemote = true;
-    new_connection.ip.assign("127.0.0.1");
-    new_connection.port.assign(std::to_string(ROOT_PORT));
-    new_connection.getaddressinformation();
-    new_connection.makesocket(false, false);
-    sockfd = new_connection.sockfd;
+    Socket sockobj;
+    string ip("127.0.0.1");
+    string port(std::to_string(ROOT_PORT));
 
     string files_buff = "";
     for (int i = 0; i < files.size(); ++i) {
         files_buff = files_buff+files[i] + "\n";
     }
 
-    if ((numbytes = sendto(sockfd, files_buff.c_str(),
-        strlen(files_buff.c_str()), 0,
-             (new_connection.ptr_address)->ai_addr,
-             (new_connection.ptr_address)->ai_addrlen)) == -1) {
-        perror("sendto");
-        exit(1);
-    }
+    sockobj.send_udp_string(ip, port, files_buff);
 
-    cout <<"Node : Sent files list to root node " <<new_connection.ip.c_str()
-        <<":" <<new_connection.port.c_str() <<endl;
+    cout <<"Node : Sent files list to root node @ " <<ip <<":" <<port <<endl;
     cout <<files_buff <<endl;
-    new_connection.cleardata();
-    close(sockfd);
 }
 
 void Node::setup_sockets(bool isRootNode) {
     struct addrinfo hints, *res;
     int sockfd;
-    Socket new_connection;
+    Socket sockobj;
+    string port(std::to_string(ROOT_PORT + node_count));
+    string ip = sockobj.get_own_ip();
 
     FD_ZERO(&readfds);
 
-    // memset(&hints, 0, sizeof hints);
-    // hints.ai_family = AF_INET;  // use IPv4 or IPv6, whichever
-    // hints.ai_socktype = SOCK_DGRAM;
-    // hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-    // getaddrinfo(NULL, "11000", &hints, &res);
-
-    // sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-    // bind(sockfd, res->ai_addr, res->ai_addrlen);
-
-    // listen for incoming UDP file list packets
     if (isRootNode) {
-        new_connection.ifUDP = true;
-        new_connection.ifRemote = false;
-        new_connection.port.assign(std::to_string(ROOT_PORT + node_count));
-        new_connection.getaddressinformation();
-        new_connection.makesocket(true, false);
-
-        // listen_udp_sock = sockfd;
-        listen_udp_sock = new_connection.sockfd;
+        listen_udp_sock = sockobj.getsocket("", port, true, false, true, false);
         maxfd = listen_udp_sock;
         FD_SET(listen_udp_sock, &readfds);
-        new_connection.ip = new_connection.get_ip();
-        cout <<"Listening on " <<new_connection.ip <<":" <<new_connection.port
-            <<endl;
-        new_connection.cleardata();
+        cout <<"Listening on " <<ip <<":" <<port <<endl;
     }
 
     // listen for incoming TCP download requests
-
-    // int sockfd2;
-    // memset(&hints, 0, sizeof hints);
-    // hints.ai_family = AF_INET;  // use IPv4 or IPv6, whichever
-    // hints.ai_socktype = SOCK_STREAM;
-    // hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-    // getaddrinfo(NULL, "11000", &hints, &res);
-
-    // sockfd2 = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-    // bind(sockfd, res->ai_addr, res->ai_addrlen);
-
-    new_connection.ifUDP = false;
-    new_connection.ifRemote = false;
-    new_connection.port.assign(std::to_string(ROOT_PORT + node_count));
-    new_connection.getaddressinformation();
-    new_connection.makesocket(true, false);
-
-    // listen_tcp_sock = sockfd2;
-    listen_tcp_sock = new_connection.sockfd;
+    listen_tcp_sock = sockobj.getsocket("", port, false, false, true, false);
     if (isRootNode)
         maxfd = listen_tcp_sock;
     else
@@ -346,6 +282,8 @@ void Node::setup_sockets(bool isRootNode) {
         exit(1);
     }
     FD_SET(listen_udp_sock, &readfds);
+    cout <<"Listening on " <<ip <<":" <<port <<endl;
+
     struct sigaction sa;
     sa.sa_handler = sigchild_handler;  // reap all the dead processes
     sigemptyset(&sa.sa_mask);
@@ -354,16 +292,10 @@ void Node::setup_sockets(bool isRootNode) {
         perror("sigaction");
         exit(1);
     }
-    new_connection.ip = new_connection.get_ip();
-    cout <<"Listening on " <<new_connection.ip <<":"<<new_connection.port <<endl;
-    new_connection.cleardata();
 }
 
 void Node::send_requested_file() {
     int rc;
-    int file_desc;
-    struct stat stat_buf;  // argument to fstat
-    off_t offset = 0;  // file offset
     struct sockaddr_storage their_addr;  // connector's address information
     socklen_t sin_size = sizeof their_addr;
     char filename[50];
@@ -385,48 +317,14 @@ void Node::send_requested_file() {
 
 
         /* get the file name from the client */
-        rc = recv(new_fd, filename, sizeof(filename), 0);
-        if (rc == -1) {
-            fprintf(stderr, "recv failed: %s\n", strerror(errno));
-            exit(1);
-        }
+        Socket sockobj;
+        string filename(sockobj.receive_tcp_string(new_fd));
 
-        /* null terminate and strip any \r and \n from filename */
-        filename[rc] = '\0';
-        if (filename[strlen(filename)-1] == '\n')
-            filename[strlen(filename)-1] = '\0';
-        if (filename[strlen(filename)-1] == '\r')
-            filename[strlen(filename)-1] = '\0';
+        string file_path("files/" + std::to_string(node_count) + "/");
+        file_path.append(filename);
 
-        /* open the file to be sent */
-        string filename_string;
-        filename_string.assign("files/");
-        filename_string.append(std::to_string(node_count).c_str());
-        filename_string.append(filename);
-        file_desc = open(filename_string.c_str(), O_RDONLY);
-        if (file_desc == -1) {
-            fprintf(stderr, "unable to open '%s': %s\n", filename,
-                strerror(errno));
-            exit(1);
-        }
+        sockobj.send_tcp_file(new_fd, file_path);
 
-        /* get the size of the file to be sent */
-        fstat(file_desc, &stat_buf);
-
-        /* copy file using sendfile */
-        offset = 0;
-        rc = sendfile(new_fd, file_desc, &offset, stat_buf.st_size);
-        if (rc == -1) {
-          fprintf(stderr, "error from sendfile: %s\n", strerror(errno));
-          exit(1);
-        }
-        if (rc != stat_buf.st_size) {
-            fprintf(stderr,
-                "incomplete transfer from sendfile: %d of %d bytes\n", rc,
-                (int)stat_buf.st_size);
-            exit(1);
-        }
-        close(file_desc);
         close(new_fd);
         exit(0);  // let child exit
     }
--- assign6/Socket.cpp
+++ assign6/Socket.cpp
@@ -16,12 +16,21 @@ Submitted by:
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
 #include <bits/stdc++.h>
+
+#include <string>
 
 #ifndef SOCKET_H
 #define SOCKET_H
 
 #define MAXDATASIZE 500
+
+using std::string;
+using std::cout;
+using std::endl;
 
 void sigchild_handler(int s) {
     while (waitpid(-1, NULL, WNOHANG) > 0) {}
@@ -38,31 +47,32 @@ class Socket{
     int returnval;
     int numbytes;
     char s[INET_ADDRSTRLEN];
-    struct sockaddr;
     struct addrinfo hints, *servinfo;
-
- public:
     int sockfd;
-    std::string ip;
-    std::string port;
-    bool ifUDP;
     bool ifRemote;
     struct addrinfo *ptr_address;
 
-    int getaddressinformation();
-    void makesocket(bool, bool);
+    void getaddressinformation(string, string, bool, bool);
     void cleardata();
-    std::string get_ip();
+ public:
+    string get_own_ip();
+    void send_udp_string(string, string, string);
+    void send_tcp_string(int, string);
+    string receive_udp_string(string);
+    string receive_tcp_string(int);
+    int getsocket(string, string, bool, bool, bool, bool);
+    void send_tcp_file(int, string);
 };
 
-int Socket::getaddressinformation() {
+void Socket::getaddressinformation(string ip, string port, bool isUDP,
+        bool isRemote) {
     // select socket type according to flag
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_protocol = 0;
-    hints.ai_socktype = (ifUDP)? SOCK_DGRAM: SOCK_STREAM;
+    hints.ai_socktype = (isUDP)? SOCK_DGRAM: SOCK_STREAM;
     // use own IP for socket if none provided
-    if (!ifRemote) {
+    if (!isRemote) {
         hints.ai_flags = AI_PASSIVE;
         returnval = getaddrinfo(NULL, port.c_str(), &hints, &servinfo);
     } else {
@@ -70,32 +80,35 @@ int Socket::getaddressinformation() {
     }
 
     if (returnval != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(returnval));
+        fprintf(stderr, "socket - getaddressinformation - getaddrinfo: %s\n",
+            gai_strerror(returnval));
         exit(1);
     }
 }
 
-void Socket::makesocket(bool ifBind, bool ifConnect) {
+int Socket::getsocket(string ip, string port, bool isUDP, bool isRemote,
+        bool ifBind, bool ifConnect) {
+    getaddressinformation(ip, port, isUDP, isRemote);
     int yes = 1;
     for (ptr_address = servinfo; ptr_address != NULL;
             ptr_address = ptr_address->ai_next) {
         if ((returnval = sockfd = socket(ptr_address->ai_family,
                 ptr_address->ai_socktype, ptr_address->ai_protocol)) == -1) {
-            perror("Socket");
+            perror("socket - getsocket - Socket");
             continue;
         }
 
         if (ifBind) {
             if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
                     sizeof(int)) == -1) {
-                perror("setsockopt");
+                perror("socket - getsocket - setsockopt");
                 exit(2);
             }
 
             if ((returnval = bind(sockfd, ptr_address->ai_addr,
                     ptr_address->ai_addrlen)) == -1) {
                 close(sockfd);
-                perror("bind");
+                perror("socket - getsocket - bind");
                 continue;
             }
         }
@@ -104,7 +117,7 @@ void Socket::makesocket(bool ifBind, bool ifConnect) {
             if (connect(sockfd, ptr_address->ai_addr,
                     ptr_address->ai_addrlen) == -1) {
                 close(sockfd);
-                perror("connect");
+                perror("socket - getsocket - connect");
                 continue;
             }
         }
@@ -113,91 +126,140 @@ void Socket::makesocket(bool ifBind, bool ifConnect) {
             inet_ntop(ptr_address->ai_family,
                 &(((struct sockaddr_in *)ptr_address->ai_addr)->sin_addr),
                 s, sizeof s);
-            printf("Connecting to remote: %s\n", s);
+            printf("socket - getsocket - Connecting to remote: %s\n", s);
         }
 
         break;
     }
 
     if (ptr_address == NULL) {
-        fprintf(stderr, "Socket::getsocket() failed\n");
+        fprintf(stderr, "socket - getsocket failed\n");
         exit(3);
     }
+    cleardata();
+    return sockfd;
 }
 
 void Socket::cleardata() {
-    ip.clear();
-    port.clear();
     freeaddrinfo(servinfo);
 }
 
-std::string Socket::get_ip() {
+std::string Socket::get_own_ip() {
     char ip4[INET_ADDRSTRLEN];
-    std::string ip;
-    struct sockaddr_in *temp = (sockaddr_in*)(
-            ptr_address->ai_addr);
-    inet_ntop(AF_INET, &(temp->sin_addr), ip4, INET_ADDRSTRLEN);
-    ip.assign(ip4);
-    return ip;
+    int sockfd;
+    void *addr;
+    char *ipver;
+    getaddressinformation("", "15000", true, false);
+    for (ptr_address = servinfo; ptr_address != NULL;
+            ptr_address = ptr_address->ai_next) {
+        if (ptr_address->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 =
+                (struct sockaddr_in*)ptr_address->ai_addr;
+            addr = &(ipv4->sin_addr);
+            break;
+        }
+    }
+    // convert ip to string and return
+    inet_ntop(AF_INET, addr, ip4, INET_ADDRSTRLEN);
+    cleardata();
+    return string(ip4);
 }
 
-void send_udp_string(std::string ipstring, std::string portno, std::string reply) {
+void Socket::send_udp_string(std::string ip, std::string port,
+        std::string reply) {
     int sockfd, numbytes;
-    Socket new_connection;
-    new_connection.ifUDP = true;
-    new_connection.ifRemote = true;
-    new_connection.ip.assign(ipstring.c_str());
-    new_connection.port.assign(portno.c_str());
-    new_connection.getaddressinformation();
-    new_connection.makesocket(false, false);
-
+    sockfd = getsocket(ip, port, true, true, false, false);
     if ((numbytes = sendto(sockfd, reply.c_str(), strlen(reply.c_str()), 0,
-             (new_connection.ptr_address)->ai_addr, (new_connection.ptr_address)->ai_addrlen)) == -1) {
-        perror("FIS_Server UDP - sendto");
+             (ptr_address)->ai_addr, (ptr_address)->ai_addrlen)) == -1) {
+        perror("socket - send_udp_string");
         exit(1);
     }
-
-    new_connection.cleardata();
     close(sockfd);
 }
 
-std::string receive_udp_string(std::string ipstring, std::string portno) {
-    Socket new_connection;
+void Socket::send_tcp_string(int tcp_socket, std::string reply) {
+    if (send(sockfd, reply.c_str(), strlen(reply.c_str()), 0) == -1) {
+        perror("socket - send_tcp_string");
+        exit(1);
+    }
+    close(sockfd);
+}
+
+std::string Socket::receive_udp_string(std::string port) {
     int sockfd, numbytes;
+    sockfd = getsocket("", port, true, false, true, false);
+    // Socket new_connection;
     struct sockaddr_storage their_addr;
     socklen_t addr_len = sizeof their_addr;
     char buf[MAXDATASIZE];
-    char s[INET6_ADDRSTRLEN];
+    char ipv4addr[INET_ADDRSTRLEN];
     std::string reply;
 
-    std::cout<<"Waiting at " <<ipstring.c_str() <<":" <<portno.c_str()<<std::endl;
-
-    new_connection.ifUDP = true;
-    new_connection.ifRemote = true;
-    new_connection.ip.assign(ipstring.c_str());
-    new_connection.port.assign(portno.c_str());
-    new_connection.getaddressinformation();
-    new_connection.makesocket(true, false);
-    new_connection.cleardata();
+    std::cout <<"Waiting at port " <<port.c_str() <<std::endl;
 
     if ((numbytes = recvfrom(sockfd, buf, MAXDATASIZE-1 , 0,
         (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-        perror("recvfrom");
+        perror("socket - receive_udp_string - recvfrom");
         exit(1);
     }
 
     inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr*)&their_addr),
-        s, sizeof s);
+        ipv4addr, sizeof ipv4addr);
 
     close(sockfd);
     buf[numbytes] = '\0';
-    reply.assign(s);
-    reply.push_back('\n');
-    reply.assign(portno);
-    reply.push_back('\n');
-    reply.append(buf);
-    std::cout <<"Received UDP string" <<std::endl;
+    reply.assign(ipv4addr + '\n' + port + '\n' + buf);
+    std::cout <<"socket - receive_udp_string - Received UDP string" <<reply
+        <<std::endl;
     return reply;
+}
+
+std::string Socket::receive_tcp_string(int tcp_socket) {
+    int rc;
+    char reply[MAXDATASIZE];
+    rc = recv(tcp_socket, reply, MAXDATASIZE, 0);
+    if (rc == -1) {
+        fprintf(stderr, "socket - receive_tcp_string - recv failed: %s\n",
+            strerror(errno));
+        exit(1);
+    }
+
+    /* null terminate and strip any \r and \n from filename */
+    reply[rc] = '\0';
+    int size = strlen(reply);
+    if (reply[size-1] == '\n' || reply[size-1] == '\r')
+        reply[size-1] = '\0';
+    return string(reply);
+}
+
+void Socket::send_tcp_file(int tcp_socket, string filename) {
+    int file_desc, rc;
+    struct stat stat_buf;
+    off_t offset = 0;
+    file_desc = open(filename.c_str(), O_RDONLY);
+    if (file_desc == -1) {
+        fprintf(stderr, "socket - send_tcp_file - unable to open '%s': %s\n",
+            filename.c_str(), strerror(errno));
+        exit(1);
+    }
+
+    /* get the size of the file to be sent */
+    fstat(file_desc, &stat_buf);
+
+    /* copy file using sendfile */
+    offset = 0;
+    rc = sendfile(tcp_socket, file_desc, &offset, stat_buf.st_size);
+    if (rc == -1) {
+        fprintf(stderr, "socket - send_tcp_file -error from sendfile: %s\n",
+            strerror(errno));
+        exit(1);
+    }
+    if (rc != stat_buf.st_size) {
+        fprintf(stderr,"socket - send_tcp_file - incomplete transfer from \
+            sendfile: %d of %d bytes\n", rc, (int)stat_buf.st_size);
+        exit(1);
+    }
+    close(file_desc);
 }
 
 #endif
